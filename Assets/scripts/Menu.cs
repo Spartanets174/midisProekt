b414e4f292d1497300d8134ee06bed9cabdd039f
Spartanets174 <99803949+Spartanets174@users.noreply.github.com>
2022-11-15 21:41:21 +0500

    Таймер

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -4,47 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
-
-
 {
-<<<<<<< HEAD
-=======
-
-    public bool CompetitiveQue;
-    public bool ControlAndStudy;
->>>>>>> parent of f4f9e31 (РўР°Р№РјРµСЂ)
-    // Start is called before the first frame update
     public void ToMenu()
     {
         SceneManager.LoadScene("Menu");
     }
     public void Atlas()
     {
-        SceneManager.LoadScene("AtlasMap");
+        SceneManager.LoadScene("Atlas");
     }
     public void Tutorial()
     {
-        ControlAndStudy = true;
-        CompetitiveQue = false;
         SceneManager.LoadScene("Tutorial");
-        Debug.Log(CompetitiveQue);
-        Debug.Log(ControlAndStudy);
-
     }
     public void Control()
     {
-        ControlAndStudy = true;
-        CompetitiveQue = false;
         SceneManager.LoadScene("Control");
-
-
     }
     public void Competitive()
     {
-        ControlAndStudy = false;
-        CompetitiveQue = true;
         SceneManager.LoadScene("Competitive");
-
     }
     public void Table()
     {
@@ -55,4 +34,6 @@ public class Menu : MonoBehaviour
         Debug.Log("Ты вышел");
         Application.Quit();
     }
+
+    
 }
--- Assets/scripts/QuizManager.cs
+++ Assets/scripts/QuizManager.cs
@@ -6,18 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class QuizManager : MonoBehaviour
 {
+    //Ссылки на др скрипты
     public List<QuestionsAnsAnswers> QnA;
     public AnswerScript answerScript;
-
-<<<<<<< HEAD
-=======
     public Menu Menu;
     //Ссылки на объекты в юнити
->>>>>>> parent of f4f9e31 (РўР°Р№РјРµСЂ)
     public GameObject next;
     public GameObject[] options;
     public GameObject[] answers;
     public GameObject[] Toggle;
+    public GameObject TextOfQuestion;
+    public GameObject QuestionImg;
+    public GameObject[] answersImg;
     public int currentQuestion;
 
     public GameObject Questions;
@@ -26,145 +26,237 @@ public class QuizManager : MonoBehaviour
 
     public Text QuestionTxt;
     public Text ResultsTxt;
-
+   //Переменные для различных целей
     public int totalQuestions = 0;
     public int rightQuestions;
     public int NumQuestion=0;
-    public bool[] isRightOption = new bool[4];
+    public int count = 0;
 
+    [SerializeField] private float time;
+    [SerializeField] private Text timerText;
+    private float _timeLeft = 180f;
     private void Start()
     {
-<<<<<<< HEAD
-        totalQuestions = QnA.Count;
-        Results.SetActive(false);
-        generateQuestion();
-    }
-    private void Update()
-    {
-       
-    }
-    public void GoMenu()
-    {
-        SceneManager.LoadScene("Menu");
-=======
-        if(Menu.CompetitiveQue == true)
+        //Установка максимального кол-ва вопросов в зависимости от режима
+        string sName = SceneManager.GetActiveScene().name;
+        if(sName == "Competitive")
         {
             totalQuestions = QnA.Count;
         }
-
-        if(Menu.ControlAndStudy == true)
+        if(sName == "Tutorial" || sName == "Control")
         {
             totalQuestions = 10;
-        }
-        
+        }        
         //Отключение окна интерфейса с результатами при старте
         Results.SetActive(false);
         generateQuestion();
-        Debug.Log(Menu.CompetitiveQue);
-        Debug.Log(Menu.ControlAndStudy);
-
->>>>>>> parent of f4f9e31 (РўР°Р№РјРµСЂ)
+        _timeLeft = time;
+        StartCoroutine(StartTimer());
     }
-
+    //Скрипты для таймера
+    private IEnumerator StartTimer()
+    {
+        while (_timeLeft > 0)
+        {
+            _timeLeft -= Time.deltaTime;
+            UpdateTimeText();
+            yield return null;
+        }
+    }
+    private void UpdateTimeText()
+    {
+        if (_timeLeft < 0)
+            _timeLeft = 0;
+        float minutes = Mathf.FloorToInt(_timeLeft / 60);
+        float seconds = Mathf.FloorToInt(_timeLeft % 60);
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+    //Проверяет правильные ли кнопки выбрал пользователь при каждом клике на кнопку и возваращет true или false для функции Answer в AnswerScript
     void onClick()
     {
+        //Проверяет есть ли ещё вопросы в тесте
         if (QnA.Count > 0)
         {
+            //Проверка сколько правильных вариантов ответа в тесте (один)
             if (QnA[currentQuestion].CorrectAnswer.Length == 1)
-            {
+            {              
+                //Цикл перебора всех кнопок с вариантами ответов
                 for (int i = 0; i < options.Length; i++)
                 {
-                    Debug.Log("Правильный ли первый: " + options[i].transform.GetChild(1).GetComponent<Toggle>().isOn + " номер первого: " + i + " правильный ответ первого: " + QnA[currentQuestion].CorrectAnswer[0]);
-                    if ((options[i].transform.GetChild(1).GetComponent<Toggle>().isOn == true) && (i + 1 == QnA[currentQuestion].CorrectAnswer[0]))
+                    //Проверка включен ли checkbox
+                    if (options[i].transform.GetChild(1).GetComponent<Toggle>().isOn == true)
                     {
-                        next.GetComponent<AnswerScript>().isCorrect = true;
-                    }
-                }
-            }
-            else
-            {
-                for (int i = 0; i < options.Length; i++)
-                {
-                    if (options[i].transform.GetChild(1).GetComponent<Toggle>().isOn == true && (i + 1 == QnA[currentQuestion].CorrectAnswer[0]))
-                    {
-                        for (int j = i + 1; j < options.Length; j++)
+                        count++;
+                        //Проверка включен ли checkbox и является ли он правильным ответов
+                        if (((options[i].transform.GetChild(1).GetComponent<Toggle>().isOn == true) && (i + 1 == QnA[currentQuestion].CorrectAnswer[0]))&&count==1)
                         {
-                            Debug.Log("Правильный ли первый: " + options[i].transform.GetChild(1).GetComponent<Toggle>().isOn + " номер первого: " + i + " правильный ответ первого: " + QnA[currentQuestion].CorrectAnswer[0] + " Правильный ли второй " + options[j].transform.GetChild(1).GetComponent<Toggle>().isOn + " номер второго: " + j + " правильный ответ второго: " + QnA[currentQuestion].CorrectAnswer[1]);
-                            if ((options[j].transform.GetChild(1).GetComponent<Toggle>().isOn == true) && (j + 1 == QnA[currentQuestion].CorrectAnswer[1]))
-                            {
-                                next.GetComponent<AnswerScript>().isCorrect = true;
-                            }
+                            //Передача true для isCorrect в скрипте AnswerScript
+                            next.GetComponent<AnswerScript>().isCorrect = true;
                         }
                     }
+                    //Debug.Log("Правильный ли первый: " + options[i].transform.GetChild(1).GetComponent<Toggle>().isOn + " номер первого: " + i + " правильный ответ первого: " + QnA[currentQuestion].CorrectAnswer[0]);                   
+                }
+            }
+            //Проверка сколько правильных вариантов ответа в тесте (два)
+            else
+            {
+                //Цикл перебора всех кнопок с вариантами ответов
+                for (int i = 0; i < options.Length; i++)
+                {
+                    count = 0;
+                    if (options[i].transform.GetChild(1).GetComponent<Toggle>().isOn == true)
+                    {                        
+                        if (options[i].transform.GetChild(1).GetComponent<Toggle>().isOn == true && (i + 1 == QnA[currentQuestion].CorrectAnswer[0]))
+                        {
+                            count++;
+                            //Цикл перебора всех кнопок с вариантами ответов для проверки второй кнопки на правильность
+                            for (int j = i + 1; j < options.Length; j++)
+                            {                              
+                                //Debug.Log("Правильный ли первый: " + options[i].transform.GetChild(1).GetComponent<Toggle>().isOn + " номер первого: " + i + " правильный ответ первого: " + QnA[currentQuestion].CorrectAnswer[0] + " Правильный ли второй " + options[j].transform.GetChild(1).GetComponent<Toggle>().isOn + " номер второго: " + j + " правильный ответ второго: " + QnA[currentQuestion].CorrectAnswer[1]);
+                                if (options[j].transform.GetChild(1).GetComponent<Toggle>().isOn == true)
+                                {
+                                    count++;
+                                    if (((options[j].transform.GetChild(1).GetComponent<Toggle>().isOn == true) && (j + 1 == QnA[currentQuestion].CorrectAnswer[1]))&&count==2)
+                                    {
+                                        next.GetComponent<AnswerScript>().isCorrect = true;
+                                    }
+                                    else
+                                    {
+                                        next.GetComponent<AnswerScript>().isCorrect = false;
+                                    }
+                                }                               
+                            }
+                        }
+                    }                  
                 }
             }
         }      
     }
+    //Скрипт для вызова onClick
     public void Checking() 
     {
         onClick();
     }
 
+    //Скрипт когда вопросов не осталось
      void GameOver()
     {
         Questions.SetActive(false); 
         Results.SetActive(true);
         ResultsTxt.text = rightQuestions + "/" + totalQuestions;
     }
-
+    //Скрипт подсчёта правильных ответов и генерации нового вопроса
     public void correct()
     {
         rightQuestions++;
-        QnA.RemoveAt(currentQuestion);
+        //Удаляет текущий вопрос, чтобы он не повторялся в будущем
+        //QnA.RemoveAt(currentQuestion);
         generateQuestion();
     }
-    
+    //Скрипт для неправильного ответа и генерации нового вопроса
     public void wrong()
     {
-        QnA.RemoveAt(currentQuestion);
+        //QnA.RemoveAt(currentQuestion);
         generateQuestion();
     }
-
+    //Скрипт для установки определённых текста/ картинок для каждого типа вопроса
     void SetAnswers()
-    {
-        for (int i = 0; i < options.Length; i++)
+    {       
+        //Если в вопросе варианты ответа и сам вопрос текстовые
+        if (QnA[currentQuestion].questionType.ToString() == "TextQuestionAndOption")
         {
-            next.GetComponent<AnswerScript>().isCorrect = false;
-            answers[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];  
+            //Строки идущие до цикла устанавливают видимость определённых частей интерфейса в зависимости от его типа, а также меняют картинку, текст и цвет текста
+            TextOfQuestion.SetActive(false);
+            QuestionImg.SetActive(false);  
+            //В цикле устанавливают видимость для элементов интерфейса, отвечающих за варианты ответа и задают им картинки/текст/цвет и т.д. в зависимости от типа вопроса
+            for (int i = 0; i < options.Length; i++)
+            {
+                answersImg[i].SetActive(true);
+                answers[i].SetActive(true);
+                answersImg[i].GetComponent<Image>().color = new Color(0, 0, 0, 0.75f);
+                answersImg[i].GetComponent<Image>().sprite = QnA[currentQuestion].ImgOfQuestion;
+                options[i].transform.GetChild(0).GetComponent<Text>().text = $"Вариант {i + 1}";
+                next.GetComponent<AnswerScript>().isCorrect = false;
+                answers[i].GetComponent<Text>().text = $"{i+1}. "+QnA[currentQuestion].Answers[i];
+            }
+        }
+        //Если в вопросе нужно дополнить текст недостающими словами
+        if (QnA[currentQuestion].questionType.ToString() == "WriteWords")
+        {
+            TextOfQuestion.SetActive(true);
+            QuestionImg.SetActive(true);
+            QuestionImg.GetComponent<Image>().sprite = QnA[currentQuestion].ImgOfQuestion;
+            QuestionImg.GetComponent<Image>().color = new Color(0, 0, 0, 0.75f);
+            TextOfQuestion.GetComponent<Text>().text = QnA[currentQuestion].Text;
+            for (int i = 0; i < options.Length; i++)
+            {
+                answersImg[i].SetActive(false);
+                answers[i].SetActive(false);
+                options[i].transform.GetChild(0).GetComponent<Text>().text = $"{i + 1}. " + $"{QnA[currentQuestion].Answers[i]}";
+                next.GetComponent<AnswerScript>().isCorrect = false;
+            }
+        }
+        //Если в вопросе сам вопрос текстовый а варианты ответа картинки
+        if (QnA[currentQuestion].questionType.ToString() == "QuestionTextOptionImg")
+        {
+            TextOfQuestion.SetActive(false);
+            QuestionImg.SetActive(false);
+            for (int i = 0; i < options.Length; i++)
+            {
+                answers[i].SetActive(false);
+                answersImg[i].SetActive(true);
+                answersImg[i].GetComponent<Image>().color = new Color(255, 255, 255, 1);
+                answersImg[i].GetComponent<Image>().sprite = QnA[currentQuestion].ImgAnswers[i];
+                options[i].transform.GetChild(0).GetComponent<Text>().text = $"Вариант {i + 1}";
+                next.GetComponent<AnswerScript>().isCorrect = false;                
+            }
+        }
+        //Если в вопросе сам вопрос картинка а варианты текстовые
+        if (QnA[currentQuestion].questionType.ToString() == "OptionTextQuestionImg")
+        {
+            TextOfQuestion.SetActive(false);          
+            QuestionImg.SetActive(true);
+            QuestionImg.GetComponent<Image>().color = new Color(255, 255, 255, 1);
+            QuestionImg.GetComponent<Image>().sprite = QnA[currentQuestion].ImgOfQuestion;
+            for (int i = 0; i < options.Length; i++)
+            {
+                answers[i].SetActive(false);
+                answersImg[i].SetActive(false);
+                options[i].transform.GetChild(0).GetComponent<Text>().text = $"{i + 1}. " + QnA[currentQuestion].Answers[i];
+                next.GetComponent<AnswerScript>().isCorrect = false;
+            }
         }
     }
+    //Скрипт для генерации нового вопроса
     void generateQuestion()
     {
-        if (QnA.Count>0)
+        //Если ещё есть вопросы в массиве
+        if (totalQuestions > NumQuestion)
         {
+            //Рандомизация вопроса
             currentQuestion = Random.Range(0, QnA.Count);
+            //Для условия вопроса
             QuestionTxt.text = QnA[currentQuestion].Question;
             NumQuestion++;
-            QuetionNum.GetComponent<Text>().text = NumQuestion.ToString();
-            for (int i = 0; i < isRightOption.Length; i++)
-            {
-                isRightOption[i] = false;
-            }
+            count = 0;
+            //Для номера вопроса
+            QuetionNum.GetComponent<Text>().text = NumQuestion.ToString();    
+            //Установление всех checkbox в отключенное состояние
             for (int i = 0; i < options.Length; i++)
             {
                options[i].transform.GetChild(1).GetComponent<Toggle>().isOn = false;
             }
             SetAnswers();
         }
+        //Если пользователь ответил на все вопросы
         else
         {
             Debug.Log("Vse");
             GameOver();
         }
-
-
     }
-
-    public enum QuestionType 
-    { 
-        TextAnswerAndOption,
-        TextAnswerOptionImg,
-        ImgAnswerOptionText
+    public void GoMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
-
 }
